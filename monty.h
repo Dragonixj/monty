7616a79e0237da50e342f57acf8a75c654e44c04
Dragonixj <delafiado@gmail.com>
2023-08-22 15:40:53 +0000

    Task 0 the push part

--- monty.h
+++ monty.h
@@ -38,5 +38,7 @@ typedef struct instruction_s
         void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
 
+void m_push(stack_t **stack, unsigned int line_number);
+
 
 #endif /* MONTY_H */
