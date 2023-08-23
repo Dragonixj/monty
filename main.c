ba4b83e7c3e123012341b5571955561855eedd22
firdawse <firdawse.guerbouzi1@gmail.com>
2023-08-23 05:39:25 -0700

    task1

--- main.c
+++ main.c
@@ -1,49 +1,70 @@
 #include "monty.h"
-
 /**
- * main - monty code interpreter
- * @argc: number of arguments
- * @argv: monty file location
- * Return: 0 on success
+ *
+ * main: monty code interpretor
+ * 
+ * Return: returns 0 on sucess
  */
+
 int main(int argc, char *argv[])
 {
-    char *content;
+    /* had to declare here because of compilation problems */
+    char buffer[1024];
+    stack_t *stack;
+    unsigned int line_number;
+    char *opcode;
     FILE *file;
-    size_t size = 0;
-    ssize_t read_line = 1;
-    stack_t *stack = NULL;
-    unsigned int counter = 0;
 
     if (argc != 2)
     {
-        fprintf(stderr, "USAGE: monty file\n");
-        exit(EXIT_FAILURE);
+        fprintf(stderr, "Usage: %s <filename>\n", argv[0]);
+        return EXIT_FAILURE;
     }
 
     file = fopen(argv[1], "r");
-    if (!file)
+    if (file == NULL)
     {
-        fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
-        exit(EXIT_FAILURE);
+        fprintf(stderr, "Error: Unable to open file %s\n", argv[1]);
+        return EXIT_FAILURE;
     }
 
-    while (read_line > 0)
-    {
-        content = NULL;
-        read_line = getline(&content, &size, file);
-        counter++;
+    stack = NULL;
+    line_number = 0;
 
-        if (read_line > 0)
+    while (fgets(buffer, sizeof(buffer), file) != NULL)
+    {
+        line_number++;
+        opcode = strtok(buffer, " \t\n");
+
+        if (opcode == NULL)
+            continue;
+
+        if (strcmp(opcode, "m_push") == 0)
         {
-            execute(content, &stack, counter, file);
+            m_push(&stack, line_number);
         }
-
-        free(content);
+        else if (strcmp(opcode, "m_pall") == 0)
+        {
+            m_pall(&stack, line_number);
+        }
+        /* Add more opcode handlers as needed */
+        else
+        {
+            fprintf(stderr, "L%u: unknown instruction %s\n", line_number, opcode);
+            fclose(file);
+            exit(EXIT_FAILURE);
+        }
     }
 
-    free_stack(stack);
     fclose(file);
-    return (0);
-}
 
+    /* Free memory and perform cleanup */
+    while (stack != NULL)
+    {
+        stack_t *temp = stack;
+        stack = stack->next;
+        free(temp);
+    }
+
+    return 0;
+}
