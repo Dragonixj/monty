7951fa84ec9d2efa8b4263b361723cf36ed62bb7
firdawse <firdawse.guerbouzi1@gmail.com>
2023-08-23 09:29:30 -0700

    task1

--- m_push.c
+++ m_push.c
@@ -1,4 +1,5 @@
 #include "monty.h"
+
 /**
  * push - Pushes an element onto the stack.
  * @stack: Pointer to the pointer of the stack's head
@@ -7,17 +8,15 @@
 void push(stack_t **stack, unsigned int line_number)
 {
     int value;
-
     stack_t *new_node;
-
     char *arg = strtok(NULL, " \t\n");
-    
+
     if (arg == NULL || !isdigit(arg[0]) || (arg[0] == '-' && !isdigit(arg[1])))
     {
         fprintf(stderr, "L%u: usage: push integer\n", line_number);
         exit(EXIT_FAILURE);
     }
-    
+
     value = atoi(arg);
     new_node = malloc(sizeof(stack_t));
     if (new_node == NULL)
@@ -25,10 +24,10 @@ void push(stack_t **stack, unsigned int line_number)
         fprintf(stderr, "Error: Memory allocation failed\n");
         exit(EXIT_FAILURE);
     }
-    
+
     new_node->n = value;
     new_node->prev = NULL;
-    
+
     if (*stack == NULL)
     {
         new_node->next = NULL;
@@ -38,6 +37,7 @@ void push(stack_t **stack, unsigned int line_number)
         new_node->next = *stack;
         (*stack)->prev = new_node;
     }
-    
+
     *stack = new_node;
 }
+
