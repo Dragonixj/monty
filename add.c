37014a9fe131fa962459698973f5ddde17f68ce9
firdawse <firdawse.guerbouzi1@gmail.com>
2023-08-24 05:25:04 -0700

    try3

--- add.c
+++ add.c
@@ -9,14 +9,13 @@
  */
 void add(stack_t **stack, unsigned int line_number)
 {
-    stack_t *current = *stack;
     
-    if (!current || !current->next)
+    if (*stack == NULL || (*stack)->next == NULL)
     {
         fprintf(stderr, "L%d: can't add, stack too short\n", line_number);
         exit(EXIT_FAILURE);
     }
     
-    current->next->n += current->n; 
+    (*stack)->next->n += (*stack)->n; 
     pop(stack, line_number); 
 }
