89d66a2ab59439dec757996130576845dce037ac
firdawse <firdawse.guerbouzi1@gmail.com>
2023-08-24 05:30:40 -0700

    task5

--- main.c
+++ main.c
@@ -63,6 +63,10 @@ int main(int argc, char *argv[])
         {
             swap(&stack, line_number);
         }
+	else if(strcmp(opcode, "nop") == 0)
+        {
+            nop(&stack, line_number);
+        }
 	else
         {
             fprintf(stderr, "L%u: unknown instruction %s\n", line_number, opcode);
