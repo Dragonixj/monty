bafd79ea108a2ae2a387f20653b976d2a40fa557
firdawse <firdawse.guerbouzi1@gmail.com>
2023-08-24 02:44:13 -0700

    task3

--- swap.c
+++ swap.c
@@ -5,24 +5,23 @@
  * @stack: A pointer to the top of the stack.
  * @line_number: The line number in the script where the swap operation is called.
  *
- * Description: This function swaps the values of the top two elements .
+ * Description: This function swaps the 2 values of the top.
  */
-
 void swap(stack_t **stack, unsigned int line_number)
 {
-	stack_t *current;
-	int new_stack;
+    stack_t *current;
+    int new_stack;
 
-	current = *stack;
+    current = *stack;
 
-	if (!current || !(current->next))
-	{
-		fprintf(stderr, "L%d: can't swap, stack too short\n", line_number);
-		exit(EXIT_FAILURE);
-	}
+    if (!current || !(current->next))
+    {
+        fprintf(stderr, "L%d: can't swap, stack too short\n", line_number);
+        exit(EXIT_FAILURE);
+    }
 
-	new_stack = current->n;
-	current->n = current->next->n;
-	current->next->n = new_stack;
+    new_stack = current->n;
+    current->n = current->next->n;
+    current->next->n = new_stack;
 }
 
